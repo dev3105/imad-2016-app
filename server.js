6b217de006f11eebcba5416c3ae16e52a7ce8484
dev3105 <priytosh.revolution@live.com>
2016-09-19 23:23:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(_dirname,'ui','article-one.html'));
+      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+
 
 app.get('/article-two', function (req, res) {
   res.send('Article two is served here');
